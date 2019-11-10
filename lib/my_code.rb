8d26573bb53d77de79754aa63742f76cb5b59d44
Charlie Broom <broomc425@gmail.com>
2019-11-10 01:28:05 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
 # Your Code Here
+def map(arr)
+  n = 0
+  newArr = []
+  while n < arr.length do
+    newArr.push( yield( arr[n] ))
+    n += 1
+  end
+  newArr
+end
+
+def reduce(arr, start = 0) 
+  n = 0
+  value = start
+  while n < arr.length do
+    value = yield( arr[n], value)
+    n += 1
+  end
+  value != nil ? value : false
+end
