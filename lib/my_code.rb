4d19477828a42e0fac37927c17d1ae64c1483bfe
Alice Won <alicewon4@gmail.com>
2019-11-14 09:03:21 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # Your Code Here
+def map(source_array)
+  #map returns an array with all values made negative
+  new_array = []
+  i = 0
+  while i < source_array.count do
+    #new_array<<source_array.[i] * -1
+    new_array.push(yield(source_array[i]))
+    i += 1
+  end
+  new_array
+end
+
+
+def reduce(source_array, sv=nil)
+  if sv
+    sum = sv
+    i = 0
+  else
+    sum = source_array[0]
+    i = 1
+  end
+
+  while i < source_array.length
+    sum = yield(sum, source_array[i])
+    i += 1
+  end
+  sum
+end
